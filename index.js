c9fef033ebf8411867ae3f1e034070295aff5d9b
sai <sai@digital-lync.com>
2021-02-09 08:02:14 +0530

    added DB Connection code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var express = require('express');
+var app= express();
 
+var DB= require('./config/db.config');
+
+DB.connect();
+
+app.listen(9011, () => {
+    console.log("server started");
+})
